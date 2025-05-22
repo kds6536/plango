11c843f13f4f644b3fca43ab9cb83697da89b6a8
kds6536 <kds6536@naver.com>
2025-05-22 14:58:50 +0900

    Figma 기반 디자인 및 코드 수정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/plango', // 이 줄이 중요! (GitHub Pages 하위폴더용)
+}
+module.exports = nextConfig
