632fad23f210c1997023bd81c075685fb187e377
kds6536 <kds6536@naver.com>
2025-06-04 14:20:19 +0900

    fix: next.js webpack alias(@lib, @hooks, @) 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -8,6 +10,12 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@lib'] = path.resolve(__dirname, 'lib');
+    config.resolve.alias['@hooks'] = path.resolve(__dirname, 'hooks');
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   },
 }
 
